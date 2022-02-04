5184e4b579157d129e7b1232a20830317f36a382
emily-nelson <94139746+emily-nelson@users.noreply.github.com>
2022-02-04 11:58:54 +0000

    Create list of dates
    Creates date using format 'dd, mm, yyyy' inside a list with specified range

--- Main.py
+++ Main.py
@@ -1,1 +1,8 @@
-# here's some text
+import datetime
+
+a = datetime.datetime.today().date()
+numdays = 10
+date_list = [a + datetime.timedelta(days=x) for x in range(numdays)]
+#print(date_list[1].strftime('%d, %m, %Y'))
+dates = [date.strftime('%d, %m, %Y') for date in date_list]
+print(dates)
