645d01c9b7ba0005e40d60c20c322d567f46803f
emily-nelson <94139746+emily-nelson@users.noreply.github.com>
2022-02-13 16:32:18 +0000

    Completed Wordle game version 1

--- Main.py
+++ Main.py
@@ -1,22 +1,55 @@
 import Words
 import random
 
-solution = random.choice(Words.words)
+play = True
 
-guessing_a_word = True
-while guessing_a_word:
-    guess = input("Guess a 5 letter word: ")
-    if len(guess) == 5:
-        if guess in Words.words:
-            guessing_a_word = False
-        else:
-            print("Guess not in word list!")
-            continue
-    else:
-        print("Guess must contain 5 letters!") 
+while play:
+    solution = random.choice(Words.words)
+    print(solution)
+    win = False
+    tries = 6
     
+    while tries > 0 and not win:
 
+        if tries == 1:
+            print("You have", str(tries), "try remaining!\n")
+        else: 
+            print("You have", str(tries), "tries remaining!\n")
 
-#take user input 
-#is this letter in the word
-#is it in the right position
+        guessing_a_word = True
+
+        while guessing_a_word:
+            guess = input("Guess a 5 letter word: ")
+            if guess == solution:
+                win = True
+                break
+            if len(guess) == 5:
+                if guess in Words.words:
+                    guessing_a_word = False
+                else:
+                    print("Guess not in word list!")
+                    continue
+            else:
+                print("Guess must contain 5 letters!") 
+        
+        for i in range(5):
+            if guess[i] == solution[i]:
+                print(guess[i] + " - Green")
+            elif guess[i] in solution:
+                print(guess[i] + " - Orange") 
+            else:
+                print(guess[i] + " - Incorrect")
+        
+        tries -= 1
+
+    if win:
+        print("You WIN!")
+    else:
+        print("You LOSE!")
+
+    play_again = input("Play again? Y/N: ").upper()
+    if play_again  == "Y":
+        continue
+    elif play_again == "N":
+        play = False  
+        
